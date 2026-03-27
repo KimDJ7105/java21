63d5a6729b39a092d3d6adb6b2aa6d85bdbdc184
Kim dong jae <rlaehdwo1225@naver.com>
2026-03-27 14:34:10 +0900

    add 00_Hello witch contains Hello.java

---
+++ 00_Hello/src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.print("안녕하세요");
+		System.out.println(1 + 2);
+		System.out.printf("이름 : %s \n나이 : %d 살 \n성별 : %c \n키 : %.2f \n학습중 : %b", "김동재", 26, '남', 180.2,true);
+	}
+}
