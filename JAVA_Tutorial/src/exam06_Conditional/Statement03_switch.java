b86a9a92ad02feb8e8062874ba1817e9bf17b75e
Kim dong jae <rlaehdwo1225@naver.com>
2026-03-31 11:40:10 +0900

    add switch example

---
+++ JAVA_Tutorial/src/exam06_Conditional/Statement03_switch.java
@@ -0,0 +1,33 @@
+package exam06_Conditional;
+
+public class Statement03_switch {
+
+	public static void main(String[] args) {
+		int num = 10;
+		
+		switch (num) {
+		case 5 : System.out.println("5");
+		break;
+		case 10 : System.out.println("10");
+		break;
+		case 15 : System.out.println("15");
+		break;
+		case 20 : System.out.println("20");
+		break;
+		default : System.out.println("default");
+		break;
+		}
+		
+		//switch expression
+		int n = 10;
+		int result = switch (n) {
+		case 10 -> 100;
+		case 15 -> 150;
+		case 20 -> 200;
+		default -> 0;
+		};
+		
+		System.out.println(result);
+	}
+
+}
