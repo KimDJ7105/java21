5d512284d324b962a1bdd0da0b239ad1c0ef3907
Kim dong jae <rlaehdwo1225@naver.com>
2026-04-13 14:28:36 +0900

    update generic exam

---
+++ JAVA_Tutorial/src/exam20_Generic/GenericTest3_super_extends.java
@@ -0,0 +1,41 @@
+package exam20_Generic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Pet {}
+class Dog extends Pet {}
+class Cat extends Pet {}
+
+public class GenericTest3_super_extends {
+
+	public static void main(String[] args) {
+		// 타입이 같거나 부모 또는 같거나 자식으로 제한하는 방법
+		
+		// <? super T>
+		// <? extends T>
+		
+		List<Cat> list = new ArrayList<Cat>();
+		List<Dog> list2 = new ArrayList<Dog>();
+		List<Pet> list3 = new ArrayList<Pet>();
+		
+		list.add(new Cat());
+		list.add(new Cat());
+		list2.add(new Dog());
+		list2.add(new Dog());
+		list3.add(new Pet());
+		list3.add(new Pet());
+		
+		printData(list);
+		printData(list3);
+		//printData(list2);
+		//사용 불가
+	}
+	
+	//전달하는 데이터 타입 및 부모만 가능하게 하기
+	//Cat 혹은 Cat의 부모만 가능
+	public static void printData(List<? super Cat> list) {
+		System.out.println(list);
+	}
+	
+}
