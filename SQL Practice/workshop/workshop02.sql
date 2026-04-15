17a5770883194c4b708f0fd3e4ba30f4921bc70d
Kim dong jae <rlaehdwo1225@naver.com>
2026-04-15 17:30:54 +0900

    add workshop 2-2 sol

--- SQL Practice/workshop/workshop02.sql
+++ SQL Practice/workshop/workshop02.sql
@@ -24,3 +24,19 @@ FROM TB_GRADE WHERE STUDENT_NO = 'A112113' GROUP BY SUBSTR(TERM_NO, 1, 4) ORDER 
 select department_no as '학과코드', count(*) from tb_student where ABSENCE_YN = 'Y' group by DEPARTMENT_NO;
 
 select student_name as '동일이름', count(*) as '동명인 수' from tb_student group by student_name having count(*) > 1;
+
+-- ////////////////////
+select count(*) as '학생 수' from tb_student where department_no = '003' and entrance_date LIKE '2001%'group by DEPARTMENT_NO;
+
+select category as '계열', department_name as '학과이름', capacity as '정원' from tb_department where category = '공학' and CAPACITY between 20 and 30 order by department_name;
+
+select category as '계열', count(*) as '학과 수' from tb_department where instr(category, '학') > 0 group by category; 
+
+-- 002
+select professor_name as '교수 이름', left(professor_ssn, 2) as '출생년도', professor_address as '주소' 
+from tb_professor where DEPARTMENT_NO = '002'
+order by left(professor_ssn, 2);
+
+select department_no as '학과번호', student_name as '학생이름', if(absence_yn='Y', '휴학', '정상') as '휴학여부' 
+from tb_student
+where department_no = '001' and instr(student_address, '서울') > 0;
